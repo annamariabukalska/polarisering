5f3962e5f7b3aa7c2f4c3390ddc461706ee1fea5
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2020-10-20 15:00:17 +0200

    fikset fish

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,20 @@
 function addAnimation() {
-  let text1 = document.querySelector('.title2');
+  let title1 = document.querySelector('.title2');
+  let text1 = document.querySelector('.section-set2');
+ 
+title1.innerHTML = title1.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-  text1.innerHTML = text1.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-  
-  anime.timeline({loop: true})
+text1.innerHTML = text1.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+ 
+anime.timeline({loop: true})
     .add({
-      targets: '.title2 .letter',
+      targets: '.title2 .letter' + '.section-set2 .letter',
       opacity: [0,1],
       easing: "easeInOutQuad",
       duration: 40,
       delay: (el, i) => 150 * (i+1)
     }).add({
-      targets: '.title2',
+      targets: '.title2' + '.section-set2',
       opacity: 0,
       duration: 750,
       easing: "easeOutExpo",
@@ -22,16 +25,22 @@ function addAnimation() {
 addAnimation()
 
 
-const sec12 = document.querySelector("#1")
-const sec12 = document.querySelector("#2")
-const sec12 = document.querySelector("#3")
-const sec12 = document.querySelector("#4")
-const sec12 = document.querySelector("#5")
-const sec12 = document.querySelector("#6")
 
-function vis() {
+
+
+
+
+
+// const sec12 = document.querySelector("#1")
+// const sec12 = document.querySelector("#2")
+// const sec12 = document.querySelector("#3")
+// const sec12 = document.querySelector("#4")
+// const sec12 = document.querySelector("#5")
+// const sec12 = document.querySelector("#6")
+
+// function vis() {
     
-}
+// }
 
-sec12.onclick = vis
+// sec12.onclick = vis
 
